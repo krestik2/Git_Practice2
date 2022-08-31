59dc03cac6b1ffcf70680eb3bd90ddc58487e7a1
Kristina <kuukristina@gmail.com>
2022-08-31 16:35:06 -0400

    created Local class

--- src/Local.java
+++ src/Local.java
@@ -1,9 +1,9 @@
 
+public class Local {
 
     int a = 10;
-         
-         double b = 2.99;
-         
-         boolean x = true;
 
-String h = "animal";
+    double b = 2.99;
+
+    boolean x = true;
+}
